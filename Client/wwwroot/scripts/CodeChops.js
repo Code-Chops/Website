963ccdbd4c58f9c87ae02875c424ab99cc009d52
Max Bergman <maxbergman@outlook.com>
2022-02-09 13:06:04 +0100

    Fixed splash screen

---
+++ Client/wwwroot/scripts/CodeChops.js
@@ -0,0 +1,38 @@
+﻿// Format and beautify code.
+window.highlightCode = () => {
+    document.querySelectorAll("pre code").forEach((element) => {
+        hljs.highlightElement(element);
+    });
+}
+
+// Register the click on the splash screen.
+document.addEventListener("click", onClick);
+
+// Remove click event listener. Fire click event and so .NET registers it.
+function onClick(doc, e) {
+    document.removeEventListener("click", onClick);
+    const element = document.getElementById("clickOverlay");
+    fireEvent(element, "click");
+}
+
+// Fire event.
+function fireEvent(element, eventType) {
+    if (element.fireEvent) {
+        element.fireEvent("on" + eventType);
+    } else {
+        const eventObject = document.createEvent("Events");
+        eventObject.initEvent(eventType, true, false);
+        element.dispatchEvent(eventObject);
+    }
+}
+
+// Remove splash, attach logo, and dissolve overlay.
+// Gets fired from .NET.
+window.onSplashClick = () => {
+    const logo = document.getElementById("logo");
+    logo.classList.add("attachedLogo");
+    logo.classList.remove("splashLogo");
+
+    const overlay = document.getElementById("overlay");
+    overlay.classList.add("dissolveOverlay");
+}
