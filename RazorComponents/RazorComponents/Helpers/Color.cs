73092f0d82194f0d9f29e5ad085faf680e232905
Max Bergman <maxbergman@outlook.com>
2022-02-11 11:13:57 +0100

    Added razorcomponents to repo

---
+++ RazorComponents/RazorComponents/Helpers/Color.cs
@@ -0,0 +1,49 @@
+﻿using ColorHelper;
+
+namespace CodeChops.Website.RazorComponents.Helpers;
+
+public record struct Color
+{
+	/// <summary>
+	/// HEX representation.
+	/// </summary>
+	public override string ToString() => this.Hex;
+
+	public string Hex { get; }
+	
+	/// <summary>
+	/// Format: "{r}, {g}, {b}"
+	/// </summary>
+	public string Rgb { get; }
+
+	public Color(byte r, byte g, byte b)
+		: this(new RGB(r, g, b))
+	{
+	}
+
+	public Color(RGB rgbColor)
+		: this(ColorConverter.RgbToHex(rgbColor), rgbColor)
+	{
+	}
+
+	public Color(string hexColor)
+		: this(new HEX(hexColor))
+	{
+	}
+
+	public Color(HEX hexColor) 
+		: this(hexColor, ColorConverter.HexToRgb(hexColor))
+	{
+	}
+
+	public Color(ColorName colorName)
+		: this(colorName.ToHex(), colorName.ToRgb())
+	{
+	}
+
+	private Color(HEX hexColor, RGB rgbColor)
+	{
+		this.Hex = $"#{hexColor}";
+		this.Rgb = $"{rgbColor.R}, {rgbColor.G}, {rgbColor.B}";
+	}
+}
---
+++ RazorComponents/RazorComponents/Helpers/RazorHelpers.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeChops.Website.RazorComponents.Helpers;
+
+public static class RazorHelpers
+{
+    public static string? If(bool predicate, string output)
+    {
+        return predicate is true ? output : null;
+    }
+}
