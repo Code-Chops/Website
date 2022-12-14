691cddbe4560a5df4f10a7a7ad478a65f285d3d7
Max Bergman <maxbergman@outlook.com>
2022-12-14 16:43:56 +0100

    0.15.5: Smaller font when smaller than 500px in width. Fixed menu bar for Pixel 4. Removed (copying of) documentation files. Removed DeepL support.

--- Server/Controllers/ProjectsController.cs
+++ Server/Controllers/ProjectsController.cs
@@ -1,5 +1,4 @@
-﻿using DeepL;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace CodeChops.Website.Server.Controllers;
@@ -8,7 +7,6 @@ public class ProjectsController : Controller
 {
 	private HttpClient HttpClient { get; }
 	private static IMemoryCache MemoryCache { get; } = new MemoryCache(new MemoryCacheOptions { ExpirationScanFrequency = TimeSpan.FromHours(1) });
-	private static Translator Translator { get; } = new(authKey: "9b5c886e-ef06-7fc4-6fb5-8ec9e80c06c3:fx");
 	
 	public ProjectsController(HttpClient httpClient)
 	{
@@ -22,10 +20,6 @@ public class ProjectsController : Controller
 		{
 			entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromDays(1);
 			var documentation = await this.HttpClient.GetStringAsync($"https://raw.githubusercontent.com/Code-Chops/{project}/master/README.md");
-			// var targetLanguageCode = new LanguageCode(languageCode);
-			//
-			// if (targetLanguageCode != LanguageCodeCache.DefaultLanguageCode)
-			// 	documentation = (await Translator.TranslateTextAsync(documentation, sourceLanguageCode: LanguageCodeCache.DefaultLanguageCode.GetLanguageCode().ToUpperInvariant(), languageCode)).Text;
 
 			return documentation;
 		}));
